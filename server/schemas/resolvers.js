b8e2bd6cd81c4e43db72ca1770764ea4c4223cce
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-12 17:57:45 -0500

    Added a mutation for login and addUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
 
 const resolvers = {
     Query: {
@@ -27,6 +28,28 @@ const resolvers = {
         // Get thought by id
         thought: async (parent, { _id }) => {
             return Thought.findOne({ _id });
+        }
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+
+            return user;
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email }); 
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            return user;
         }
     }
 };
