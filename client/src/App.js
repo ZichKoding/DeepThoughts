a1770841a74f7d2fb251446a32f4a5cc8e0ea77f
Chris Zichko <chriszichkocoding@gmail.com>
2022-01-17 13:40:14 -0500

    break time at mod 21.5.6

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,21 @@ import NoMatch from './pages/NoMatch';
 import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token? `Bearer ${token}`: '',
+    },
+  };
 });
 
 const client = new ApolloClient({
